f71c91c66dc4515d7792dfbc32805fe187326e82
Mostafa Mahmod <mostaffa201021@gmail.com>
2026-09-11 17:38:48 +0300

    auto: apply ascpt-clinic-pwa-v1_4_0_beta28.tar.gz [2026-09-11 17:38]

--- js/clinic-config.js
+++ js/clinic-config.js
@@ -10,7 +10,7 @@ export const CLINIC_CONFIG = {
  englishName: 'Alexandria Specialized Center for Physical Therapy',
  abbreviation: 'ASCPT',
  tagline: 'نظام إدارة مراكز وعيادات العلاج الطبيعي',
- version: '1.4.0-beta.27',
+ version: '1.4.0-beta.28',
  director: {
   name: 'د. حسني أحمد الجويلي',
   title: 'إستشاري العلاج الطبيعي والتقويم الحركي للعمود الفقري'
