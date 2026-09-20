ce94a8c39d11762c19bc82d63d88685a20b2ba3f
Mostafa Mahmod <mostaffa201021@gmail.com>
2026-09-20 21:22:29 +0300

    refactor(css): phase 3 sector 8 - isolate insurance claims and attendance cards view

--- sw.js
+++ sw.js
@@ -27,6 +27,7 @@ const APP_SHELL_ASSETS = [
   '/css/views/sessions.css',
   '/css/views/admin.css',
   '/css/views/finance.css',
+  '/css/views/claims.css',
   '/css/style.css',
   '/css/style.css?v=1.4.3',
   '/css/print.css',
