a9799154721e2b5aa6a77799c443eff90a82f745
Mostafa Mahmod <mostaffa201021@gmail.com>
2026-09-20 15:22:46 +0300

    feat(refactor): complete phase 3 sector 1 app shell and navigation view

--- sw.js
+++ sw.js
@@ -20,6 +20,7 @@ const APP_SHELL_ASSETS = [
   '/css/components/forms.css',
   '/css/components/modals.css',
   '/css/components/tables.css',
+  '/css/views/shell.css',
   '/css/style.css',
   '/css/style.css?v=1.4.3',
   '/css/print.css',
