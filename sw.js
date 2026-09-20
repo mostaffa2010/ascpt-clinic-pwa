faf80aef4e531279a5c90839914bcc66c7cf5486
Mostafa Mahmod <mostaffa201021@gmail.com>
2026-09-20 21:02:34 +0300

    refactor(css): phase 3 sector 7 - isolate finance and monthly reports view

--- sw.js
+++ sw.js
@@ -26,6 +26,7 @@ const APP_SHELL_ASSETS = [
   '/css/views/doctor.css',
   '/css/views/sessions.css',
   '/css/views/admin.css',
+  '/css/views/finance.css',
   '/css/style.css',
   '/css/style.css?v=1.4.3',
   '/css/print.css',
